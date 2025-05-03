663c120ce2f7d6026a06f547af324192ccd04633
D4nD4n01 <danytazos@gmail.com>
2025-05-02 23:50:30 -0600

    20250502 2350

--- server.js
+++ server.js
@@ -11,21 +11,27 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-console.log("Valores de conexión:");
-console.log("HOST:", process.env.DB_HOST);
-console.log("PORT:", process.env.DB_PORT);
-console.log("USER:", process.env.DB_USER);
-console.log("PASS:", process.env.DB_PASSWORD);
-console.log("DB:", process.env.DB_DATABASE);
+const cleanEnv = (val) => val?.trim().replace(/^=+/, "").replace(/^\t+/, "");
 
+// Limpieza de variables de entorno
+const dbConfig = {
+  host: cleanEnv(process.env.DB_HOST),
+  port: Number(cleanEnv(process.env.DB_PORT)),
+  user: cleanEnv(process.env.DB_USER),
+  password: cleanEnv(process.env.DB_PASSWORD),
+  database: cleanEnv(process.env.DB_DATABASE),
+};
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,        // ej: mysql-production-afe9.up.railway.app
-  user: process.env.DB_USER,        // ej: root
-  password: process.env.DB_PASSWORD,// contraseña
-  database: process.env.DB_DATABASE,// railway
-  port: process.env.DB_PORT,        // 3306
-});
+console.log("Valores de conexión LIMPIOS:");
+console.log("HOST:",dbConfig.host);
+console.log("PORT:",dbConfig.port);
+console.log("USER:",dbConfig.user);
+console.log("PASS:",dbConfig.password);
+console.log("DB:",dbConfig.database);
+
+// Crear conexión con valores limpios
+const pool = mysql.createPool(dbConfig);
+
 
 app.get("/", async (req, res) => {
   res.send("Servidor funcionando POR FIN; YA ERA HORA");
@@ -38,7 +44,7 @@ pool.getConnection()
 
 app.post("/login", async (req, res) => {
   const { usuario, password } = req.body;
-  console.log("Usuario recibido: ",usuario,password)
+  console.log("Usuario recibido: ", usuario, password)
 
   //console.log("req de la solicitud:", req); // Verifica lo que llega al servidor
   //console.log("res de la solicitud:", res);
@@ -55,7 +61,7 @@ app.post("/login", async (req, res) => {
     }
   } catch (error) {
     console.error("Error en login:", error);
-    res.status(500).json({ success: false, message: "Error en el servidor; no puede ingresar al login",req:req,res:res });
+    res.status(500).json({ success: false, message: "Error en el servidor; no puede ingresar al login", req: req, res: res });
   }
 });
 
